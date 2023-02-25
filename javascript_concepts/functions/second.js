678b25a4a307ae62b0779c6289d0bfdfd1f9544b
Jonathan Mohabir <jonamohabir416@gmail.com>
2023-02-25 13:58:33 -0500

    functions understood

---
+++ javascript_concepts/functions/second.js
@@ -0,0 +1,14 @@
+function findTotalAmount(number1, number2, number3) {
+  return `Your total amount is ${number1 + number2 + number3}`;
+}
+function greetSomeone(name) {
+  return `hey there ${name}`;
+}
+findTotalAmount(1, 2, 3);
+greetSomeone('Jonathan Mohabir ');
+
+const makeADeveloper = function (firstName, secondName) {
+  return `Okay ${firstName} ${secondName} you are now  React Developer good on ya `;
+};
+
+makeADeveloper('Jonathan', 'Mohabir ');

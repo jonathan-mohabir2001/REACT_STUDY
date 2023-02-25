00e4025ff5d8220308b45d1561713a443f2cf0ca
Jonathan Mohabir <jonamohabir416@gmail.com>
2023-02-25 13:02:58 -0500

    variables understood for working with react

--- javascript_concepts/variables/secondary.js
+++ javascript_concepts/variables/secondary.js
@@ -1,9 +1,23 @@
-// This js file continues the review of varibles 
-//Template Strings - needs `` to be around the statement when trying 
+// This js file continues the review of varibles
+//Template Strings - needs `` to be around the statement when trying
 
-// To work with this understand the concept of string contatination 
+// To work with this understand the concept of string contatination
 
-const firstName= "Jonathan"
-const middleName= "Bali"
-const lastName 
+const firstName = 'Jonathan';
+const middleName = 'Bali';
+const lastName = 'Mohabir';
 
+function outPutNames() {
+  console.log(
+    'Hi there my name is:',
+    firstName + ' ' + middleName + ' ' + lastName
+  );
+}
+outPutNames();
+// String contatenced function but used quotation marks for spacing
+
+function templateStringOutput() {
+  console.log(`Hi there my name is ${firstName} ${middleName} ${lastName}`);
+}
+templateStringOutput();
+// Template strings utilized.
